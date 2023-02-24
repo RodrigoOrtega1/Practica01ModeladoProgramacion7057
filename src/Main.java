3f875bacb6904b5a730d1357bae7a6cad2e88136
Rodrigo Ortega <rodrigoortega@ciencias.unam.mx>
2023-02-24 17:14:58 -0600

    Yei

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
+import java.io.IOException;
+
 public class Main{
-    public static void main(String[] args){
-        Fight fight = new Fight();
-        fight.initialize();
-        User usuario1 = new User("usuario1", fight.fighters.get(0), fight);
-        // User usuario2 = new User("usuario2", fight.fighters.get(0), fight);
-        // User usuario3 = new User("usuario3", fight.fighters.get(0), fight);
-        // User usuario4 = new User("usuario4", fight.fighters.get(0), fight);
-        fight.fight();
-        usuario1.replayFight();
+    public static void main(String[] args) throws IOException{
+        Fight fight1 = new Fight();
+        Fight fight2 = new Fight();
+        Fight fight3 = new Fight();
+        Fight fight4 = new Fight();
+        fight1.initialize();
+        fight2.initialize();
+        fight3.initialize();
+        fight4.initialize();
+        User usuario1 = new User("usuario1", fight1.fighters.get(0), fight1);
+        User usuario2 = new User("usuario2", fight2.fighters.get(1), fight2);
+        User usuario3 = new User("usuario3", fight3.fighters.get(2), fight3);
+        User usuario4 = new User("usuario4", fight4.fighters.get(1), fight4);
+        fight1.fight();
+        fight2.fight();
+        fight3.fight();
+        fight4.fight();
+        usuario1.writeLog();
+        usuario2.writeLog();
+        usuario3.writeLog();
+        usuario4.writeLog();
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,3 +1,7 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class User implements Observer{
@@ -44,5 +48,16 @@ public class User implements Observer{
         for (String fightEvents : fight){
             System.out.println(fightEvents);
         }
+    }
+
+    public void writeLog() throws IOException{
+        File file = new File("./bitacora" + id + ".txt");
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        for(int i = 0; i < fight.size(); i++){
+            bufferedWriter.write(fight.get(i).toString() + "\n");
+        }
+        bufferedWriter.flush();
+        bufferedWriter.close();
     }
 }
